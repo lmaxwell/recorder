ced09c023a2f0e27f50bac936858af7e999916f1
lmaxwell <shysian@gmail.com>
2020-06-05 00:22:44 +0800

    recorder

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { Recorder } from "./Recorder/index.js";
+import "./styles.css";
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      audioURL: null,
+      audioDetails: {
+        url: null,
+        blob: null,
+        chunks: null,
+        duration: {
+          h: 0,
+          m: 0,
+          s: 0
+        }
+      }
+    };
+  }
+  handleAudioStop(data) {
+    console.log(data);
+    this.setState({ audioDetails: data });
+    //console.log(data);
+  }
+  handleAudioUpload(file) {
+    this.setState({audioURL:null});
+
+    console.log(file);
+  }
+  handleRest() {
+    const reset = {
+      url: null,
+      blob: null,
+      chunks: null,
+      duration: {
+        h: 0,
+        m: 0,
+        s: 0
+      }
+    };
+    this.setState({ audioDetails: reset });
+  }
+  render() {
+    return (
+      <div className="App">
+        <Recorder
+          record={true}
+          title={"New recording"}
+          audioURL={this.state.audioDetails.url}
+          showUIAudio
+          handleAudioStop={data => this.handleAudioStop(data)}
+          handleAudioUpload={data => this.handleAudioUpload(data)}
+          handleRest={() => this.handleRest()}
+        />
+      </div>
+    );
+  }
+}
